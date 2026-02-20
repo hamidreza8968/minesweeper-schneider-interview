9c49a86267a7254f4903f27520f6b94d795e1622
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2026-02-20 21:05:43 +0100

    feat: integrate BoardProvider and Board into App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {BoardProvider} from "../context/BoardContext";
+import Board from "../components/Board/Board";
 
-function App() {
-  return <div>Welcome to Minesweeper!</div>;
-}
+const App: React.FC = () => {
+    return (
+        <BoardProvider rows={5} cols={5} bombRate={0.2}>
+            <Board className="app"/>
+        </BoardProvider>
+    );
+};
 
 export default App;
